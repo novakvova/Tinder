6d0ce4790360d50adf9b615705ee33dd1cc60b2e
sergijolenuk50 <sergijolenuk50@gmail.com>
2025-01-14 22:12:16 +0200

    new branch

--- my-react/src/pages/home.tsx
+++ my-react/src/pages/home.tsx
@@ -29,18 +29,17 @@ const Home = () => {
 
 return (
     <>
-        <h1>dfjaslfjgbksldfjkgjsdfkjg</h1>
 
-        <div className="h-56 sm:h-64 xl:h-80 2xl:h-96">
+        <div className="h-100% sm:h-64 xl:h-80 2xl:h-96 ">
 
-        <button data-tooltip-target="tooltip-default" type="button" className="text-white bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 font-medium rounded-lg text-sm px-5 py-2.5 text-center dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800">Default tooltip</button>
+        <button data-tooltip-target="tooltip-default" type="button" className="text-white bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 font-medium rounded-lg text-sm px-5 py-2.5 text-center dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800">Create en account</button>
 
 <div id="tooltip-default" role="tooltip" className="absolute z-10 invisible inline-block px-3 py-2 text-sm font-medium text-white transition-opacity duration-300 bg-gray-900 rounded-lg shadow-sm opacity-0 tooltip dark:bg-gray-700">
     Tooltip content
-    <div className="tooltip-arrow" data-popper-arrow></div>
+    <div className="tooltip-arrow " data-popper-arrow></div>
 </div>
-      <Carousel>
-        <img src="https://flowbite.com/docs/images/carousel/carousel-1.svg" alt="..." />
+      <Carousel className="HomeCarusel">
+        <img src="https://st5.depositphotos.com/4218696/64159/i/1600/depositphotos_641597274-stock-photo-collage-happy-multiracial-people-avatars.jpg" alt="..." />
         <img src="https://flowbite.com/docs/images/carousel/carousel-2.svg" alt="..." />
         <img src="https://flowbite.com/docs/images/carousel/carousel-3.svg" alt="..." />
         <img src="https://flowbite.com/docs/images/carousel/carousel-4.svg" alt="..." />
