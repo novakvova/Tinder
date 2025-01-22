f05a48dd6de69a3a912f1a82ee3cc1806c3f688a
volodavkaa <volodavkaa@gmail.com>
2025-01-22 13:06:34 +0200

    1

--- setup_and_run.bat
+++ setup_and_run.bat
@@ -1,17 +1,11 @@
-
-echo === Створення віртуального середовища ===
 cd backend
 python -m venv venv
 call venv\Scripts\activate
-echo === Встановлюємо залежності Python ===
+
 pip install --upgrade pip
 pip install -r requirements.txt
 
-
-echo === Встановлюємо залежності фронтенду ===
 cd ..\my-react
 npm install
 
-
-echo === Всі залежності успішно встановлені! ===
 pause
